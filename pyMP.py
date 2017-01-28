f0475ca0ed6514b16e330d2b4ef143ddf96f931c
Olga712 <icegirl712@mail.ru>
2017-01-29 02:14:00 +1300

    test

--- pyMP.py
+++ pyMP.py
@@ -1,14 +1,17 @@
-def Hello():
-    print ("Hello world")
+import math
 
-def GenTime(StartT,N,fs):
+
+def Hello():
+    print ("Hello world!!!!!!")
+
+def GenTime(StartT,N,fs)
     return np.linspace(StartT, (StartT+(N-1)/fs), N)
 
 def ImpulsGen(ImpName,time,**argIn):
     print(time)
-    if ImpName == 'Puzirev': 
+    if ImpName == 'Puzirev':
         ImpFun= lambda t: argIn['A']*math.exp(-(argIn['B']*t)**2)*math.cos(2*math.pi*argIn['f']*(t+argIn['Phi']))
-    elif ImpName == 'Berlage': 
+    elif ImpName == 'Berlage':
         ImpFun=lambda t: argIn['A']*(t**argIn['n'])*math.exp(-argIn['B']*t)*math.cos(2*math.pi*argIn['f']*t+math.pi/2)
     elif ImpName == 'Gauss':
         ImpY=argIn['A']*math.exp(-argIn['B']*t**2)*math.sin(2*math.pi*argIn['f']*t)
