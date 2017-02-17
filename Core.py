4e674d219df8bfc4ffeed38ea351fdcc9abd47ae
lex3RU <tabinvox@outlook.com>
2017-02-17 13:25:03 +0200

    1

--- Core.py
+++ Core.py
@@ -1,7 +1,7 @@
 # Ядро комплекса
 # Классы Workspace, WorkspaceObject
 import numpy as np
-
+import matplotlib.pyplot as plt
 #============================================================
 class Workspace(object):
     """description of class"""
@@ -18,15 +18,20 @@ class WorkspaceObject(object):
 class Signal_1D(WorkspaceObject):
     # Класс одномерного сигнала
     def __init__(self):
-        self.SigProp = {"SigName": "Какой-то сигнал", "fs": 1, "fsUnit": 1}  # Словарь с параметрами сигнала
-        self.Waveform = np.array([1, 2, 3])
+        self.SigProp = {"SigName": "Какой-то сигнал",
+                             "fs": 1,
+                         "fsUnit": 1,
+                        "TStart" : 0}  # Словарь с параметрами сигнала
+
+        self.Waveform = np.array([1, 2, 3]) #   Временная форма (отсчеты) сигнала
+        self.timeline = np.array([0, 1, 2]) #   Отсчеты времени TODO Сделать генератор ???
 
     def __str__(self):
         msg=self.SigProp["SigName"]
         return msg
-
+    # Загрузка сигнала из внешнего источника
     def loadSignal(self, param, SourceType = 1):
-        # Тут сделать загрузку из разных источников
+        # TODO Тут сделать загрузку из разных источников
         WF=[]
         if SourceType == 1:
             filename = param
@@ -46,19 +51,45 @@ class Signal_1D(WorkspaceObject):
                         try:
                             WF.append(float(line))
                         except:
-                            print("Error1")
+                            print("Core.loadsignal.Error1")
                             print(line)
                 self.Waveform = np.array(WF)
             except:
-                print("Error2")
+                print("Core.loadsignal.Error2")
+
+    def NormCenter(self):
+        #  Центрируем
+        self.Waveform = self.Waveform - self.Waveform.mean()
+        #  Нормируем
+        self.Waveform = self.Waveform / self.Waveform.std()
 
 
 #============================================================
 class Signal_nD(WorkspaceObject):
     # Класс многомерного сигнала
     Signals = []  # LIST of workspaceObject
+    Timeline = [] # Ось времени
+#  TODO Варианты синхронизации сигналов по времени (добавление пропусков, децимация, интерполяция)
+#  TODO Преобразование оси времени совместно со всеми сигналами
+    def __init__(self, param = {"TStart":0,"Length": 100, "fs": 1, "fsUnit": 1}):
+        #  Создаем общую для всех сигналов ось времени.
+        self.Timeline = np.linspace(param["TStart"], param["TStart"]+param["Length"]/param["fs"], param["Length"])
+        self.NDSignalParam = param
 
     def addSignal(self, newObject):
+#  TODO Добавить проверку "влезает ли сигнал в ось времени
         self.Signals.append(newObject)
 
+    def createSignal(self, n = 2):
+        # создаем N одномерных пустых сигналов и добавляем его к многомерному
+        for i in range(n):
+            self.Signals.append(Signal_1D())
 
+    def plot(self):
+
+        fig = plt.figure()
+        ax = fig.add_subplot(1, 1, 1)
+
+        for i in range(len(self.Signals)):
+            ax.plot(self.Signals[i].Waveform)
+        plt.show()
--- MainProg.py
+++ MainProg.py
@@ -2,6 +2,7 @@
 import Core
 import Global
 import matplotlib.pyplot as plt
+import copy
 
 # Загружаем глобальные параметры
 Config = Global.GlobalParam()
@@ -10,15 +11,38 @@ Config.LoadParam()
 
 # Создаем сигнал
 Sig1 = Core.Signal_1D()
-
-filename = Config['datapath'].rstrip("\\n") + "\\Radon1.txt"
-Sig1.loadSignal(filename)
+Sig1.loadSignal(Config['datapath'].rstrip("\\n") + "\\Radon1.txt")
 Sig1.SigProp["SigName"]="Radon"
-fig=plt.figure()
+Sig1.NormCenter()
 
-plt.plot(Sig1.Waveform)
-plt.show()
+Sig2 = Core.Signal_1D()
+Sig2.loadSignal(Config['datapath'].rstrip("\\n") + "\\Debet.txt")
+Sig2.SigProp["SigName"]="Debet"
+Sig2.NormCenter()
 
+Glob=Core.Signal_nD()
+Glob.addSignal(Sig1)
+Glob.addSignal(Sig2)
+fig = Glob.plot()
+
+
+"""
+plt.plot(Sig2.Waveform)  #  Рисуем сигнал
+plt.show()               #  Показываем окно с графиком
+
+
+#  ТЕСТ ГЕЙНДЖЕРА
+#  from scipy import stats
+import statsmodels.api as sm
+#  from statsmodels.graphics.api import qqplot
+
+ar_mod20 = sm.tsa.ARMA(Sig1.Waveform, (2,0)).fit(disp=False)
+print(ar_mod20.params)
+
+arx_mod20 = sm.tsa.ARMA(Sig1.Waveform, (2,0), Sig2.Waveform).fit(disp=False)
+print(ar_mod20.params)
+print(arx_mod20.params)
+"""
 
 
 
